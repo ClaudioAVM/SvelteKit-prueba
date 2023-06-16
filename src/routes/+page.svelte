6e5fb79532f5b04c4d08f3134ad7d20bdebf695a
Claudio Valenzuela <claudiovalenzuela89@hotmail.com>
2023-06-16 14:26:26 -0400

    files

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+
+<script>
+let coins = [];
+
+  const loadCoins = async () => {
+    const res = await fetch(
+      `http://www.claudiovm.somee.com/api/AccesoTodos`
+    );
+    const data = await res.json();
+    console.log(data);
+
+    coins = data;
+
+  };
+
+  loadCoins();
+</script>
+
+<h1>Hola</h1>
+
+{#each coins as coin, i}
+   {coin.username}
+   <br>
+{/each}
+
